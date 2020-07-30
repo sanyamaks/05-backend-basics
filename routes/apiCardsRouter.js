a3f396d4a1ed0718402db64a581c2ed39ade1c60
Aleksandr <sanyamaks14@gmail.com>
2020-07-30 22:07:47 +0400

    Fix .eslint.js, Add status 404 for apiUsersRouter

--- routes/apiCardsRouter.js
+++ routes/apiCardsRouter.js
@@ -4,10 +4,11 @@ const path = require('path');
 
 apiCardsRouter.get('/cards', (req, res) => {
   const cardsPath = path.join(__dirname, '..', 'data', 'cards.json');
-  const cardsReadFilePromises = fs.promises.readFile(cardsPath, {
-    encoding: 'utf8',
-  });
-  cardsReadFilePromises
+
+  fs.promises
+    .readFile(cardsPath, {
+      encoding: 'utf8',
+    })
     .then((data) => {
       res.writeHead(200, {
         'Content-Type': 'application/json',
@@ -15,24 +16,8 @@ apiCardsRouter.get('/cards', (req, res) => {
       res.end(data);
     })
     .catch(() => {
-      res.status(404).send({ message: 'Internal Server Error' });
+      res.status(500).send({ message: 'Internal Server Error' });
     });
-
-  // const cardsStream = await fs.createReadStream(cardsPath, { encoding: 'utf8' });
-  //
-  // cardsStream.on('open', () => {
-  //   res.writeHead(200, {
-  //     'Content-Type': 'application/json',
-  //   });
-  // });
-  //
-  // cardsStream.on('error', () => {
-  //   res.statusCode = 500;
-  //   res.statusMessage = 'Internal Server Error';
-  //   res.send('500 Internal Server Error');
-  // });
-  //
-  // cardsStream.pipe(res);
 });
 
 module.exports = apiCardsRouter;
--- routes/apiUsersRouter.js
+++ routes/apiUsersRouter.js
@@ -4,10 +4,11 @@ const path = require('path');
 
 apiUsersRouter.get('/users', (req, res) => {
   const usersPath = path.join(__dirname, '..', 'data', 'users.json');
-  const usersReadFilePromises = fs.promises.readFile(usersPath, {
-    encoding: 'utf8',
-  });
-  usersReadFilePromises
+
+  fs.promises
+    .readFile(usersPath, {
+      encoding: 'utf8',
+    })
     .then((data) => {
       res.writeHead(200, {
         'Content-Type': 'application/json',
@@ -21,17 +22,15 @@ apiUsersRouter.get('/users', (req, res) => {
 
 apiUsersRouter.get('/users/:id', (req, res) => {
   const usersPath = path.join(__dirname, '..', 'data', 'users.json');
-  const usersReadFilePromises = fs.promises.readFile(usersPath, {
-    encoding: 'utf8',
-  });
-  usersReadFilePromises
+
+  fs.promises
+    .readFile(usersPath, {
+      encoding: 'utf8',
+    })
     .then((users) => JSON.parse(users).filter((item) => item._id === req.params.id))
     .then((searchedUser) => {
       if (searchedUser.length === 0) {
-        res.writeHead(200, {
-          'Content-Type': 'application/json',
-        });
-        res.end(JSON.stringify({ message: 'Нет пользователя с таким id' }));
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
         return;
       }
       res.writeHead(200, {
@@ -40,7 +39,7 @@ apiUsersRouter.get('/users/:id', (req, res) => {
       res.end(JSON.stringify(searchedUser));
     })
     .catch(() => {
-      res.status(404).send({ message: 'Internal Server Error' });
+      res.status(500).send({ message: 'Internal Server Error' });
     });
 });
 
